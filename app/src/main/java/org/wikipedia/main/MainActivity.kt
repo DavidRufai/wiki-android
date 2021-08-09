2f5be7264984d741b98f886d04679a41bbf4d457
david <davidr@pointzi.com>
2021-08-09 18:02:41 +0100

    pointzi base

--- app/src/main/java/org/wikipedia/main/MainActivity.kt
+++ app/src/main/java/org/wikipedia/main/MainActivity.kt
@@ -8,6 +8,7 @@ import android.view.View
 import androidx.appcompat.view.ActionMode
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
+import com.pointzi.Pointzi
 import org.wikipedia.Constants
 import org.wikipedia.R
 import org.wikipedia.WikipediaApp
@@ -56,6 +57,7 @@ class MainActivity : SingleFragmentActivity<MainFragment>(), MainFragment.Callba
         supportActionBar?.title = ""
         supportActionBar?.setDisplayHomeAsUpEnabled(false)
         binding.mainToolbar.navigationIcon = null
+        Pointzi.setUserId("xheghun-test-build")
     }
 
     override fun onResume() {
